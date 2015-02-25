1ceefcfa51353bc64f1819e6186d23d7ebce9629
Gordon RASS-KWASI <grass-kw@e1r2p18.42.fr>
2015-02-25 11:40:50 +0100

    test_swap et tri_inverse Ok

--- algo.c
+++ algo.c
@@ -6,7 +6,7 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/17 13:14:56 by grass-kw          #+#    #+#             */
-/*   Updated: 2015/02/24 18:27:06 by grass-kw         ###   ########.fr       */
+/*   Updated: 2015/02/25 11:39:23 by grass-kw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,29 +57,26 @@ void		tri_selection(t_env *e)
 
 int			test_swap(t_env *e)
 {
-	(void)e;
-	// ft_swap(&(e->a));
-	// if (ft_pltest(&(e->a), isdecroissant))
-	// {
-	// 	ft_opback(&(e->list_operator), ft_opnew("sa"));
-	// 	ft_print(&(e->a));
-	// 	return (1);
-	// }
-	// ft_swap(&(e->a));
+	t_pile	*tmp;
+
+	tmp = ft_plcpy(e->a);
+	ft_swap(&tmp);
+	if (ft_pltest(&tmp, isdecroissant))
+	{
+		ft_opback(&(e->list_operator), ft_opnew("sa"));
+		return (1);
+	}
+	ft_piledel(&tmp);
 	return (0);
 }
 
 int			tri_inverse(t_env *e)
 {
-	(void)e;
-	// t_pile	*tmp;
-
-	// (void)tmp;
-	// tmp = ft_reverse(&(e->a));
-	// ft_swap(&(e->a));
-	// ft_opback(&(e->list_operator), ft_opnew("ra"));
-	// ft_opback(&(e->list_operator), ft_opnew("sa"));
-	// ft_print(&(e->a));
+	ft_reverse(&(e->a));
+	ft_swap(&(e->a));
+	ft_opback(&(e->list_operator), ft_opnew("ra"));
+	ft_opback(&(e->list_operator), ft_opnew("sa"));
+	ft_print(&(e->a));
 	 return (1);
 }
 
--- pushswap.c
+++ pushswap.c
@@ -6,7 +6,7 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/18 10:58:14 by grass-kw          #+#    #+#             */
-/*   Updated: 2015/02/25 11:25:16 by grass-kw         ###   ########.fr       */
+/*   Updated: 2015/02/25 11:39:45 by grass-kw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,27 @@ static void	parsing(char **argv, t_env *e)
 	}
 }
 
-// static void	algo(t_env *e)
-// {
-// 	if (ft_pltest(&(e->a), isdecroissant))
-// 		return ;
-// 	// else if (ft_plsize(&(e->a)) == 3 && ft_pltest(&(e->a), iscroissant))
-// 	// 	{
-// 	// 		tri_inverse(e);
-// 	// 		return ;
-// 	// 	}
-// 	// else if (test_swap(e))
-// 	// 	return ;
-// 	// else if (test_rotate(e))
-// 	// 	return ;
-// 	// else if (test_rrotate(e))
-// 	// 	return ;
-// 	else
-// 	{
-// 		// ft_print(&(e->a));
-// 		tri_selection(e);
-// 	}
-// }
+static void	algo(t_env *e)
+{
+	if (ft_pltest(&(e->a), isdecroissant))
+		return ;
+	else if (ft_plsize(&(e->a)) == 3 && ft_pltest(&(e->a), iscroissant))
+		{
+			tri_inverse(e);
+			return ;
+		}
+	else if (test_swap(e))
+		return ;
+	// else if (test_rotate(e))
+	// 	return ;
+	// else if (test_rrotate(e))
+	// 	return ;
+	else
+	{
+		// ft_print(&(e->a));
+		tri_selection(e);
+	}
+}
 
 void	pushswap(char **argv)
 {
@@ -60,7 +60,7 @@ void	pushswap(char **argv)
 	e.b = NULL;
 	e.list_operator = NULL;
 	parsing(argv, &e);
-	// algo(&e);
-	// ft_opprint(e.list_operator);
+	algo(&e);
+	ft_opprint(e.list_operator);
 	envdel(&e);
 }
